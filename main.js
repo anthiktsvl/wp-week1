cfe9c1e731ddb0aede6b144b160926e5393cb37f
Anthi <115115836+anthiktsvl@users.noreply.github.com>
2024-10-17 17:02:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const h1 = document.querySelector('h1');
+console.log(h1)
 
+let counter =0;
+
+function increase(){
+    counter++;
+    h1.textContent = counter;
+}
+
+setInterval( increase , 1000);
